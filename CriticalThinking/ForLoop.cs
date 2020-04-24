886129bc5924b39ff6ac2443aa116b1e2497c018
DavidRoszina <david.a.roszina@gmail.com>
2020-04-24 08:32:51 -0500

    Created loop that runs 5 times

---
+++ CriticalThinking/ForLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking
+{
+    class ForLoop
+    {
+        //Member Variables
+
+        //Constructor
+        public ForLoop()
+        {
+            for (int i = 0; i < 6; i++) ;
+        }
+
+        //Methods
+    }
+}
